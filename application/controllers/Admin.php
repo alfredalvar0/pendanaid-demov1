4574738a22aa6278cb83e1bcb3886d5aa3742f12
krisna <krisna.raksa@ciputra.com>
2021-11-10 20:17:17 +0700

    fitur reset password
    Perbaikan fitur reset password untuk investor dan penambahan fitur reset
    password untuk admin

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -21,7 +21,7 @@ class Admin extends CI_Controller {
 			$this->load->view('admin/login');
 		}
 		// $this->load->view('login');
-		
+
 	}
 
 	public function reset_pass()
@@ -37,29 +37,92 @@ class Admin extends CI_Controller {
 			$data['content'] = 'admin/dashboard';
 			$data['history']=$this->m_invest->dataDanaHistoryTransaksiAdmin();
 			$this->load->view('admin/indexadmin',$data);
-			
+
 		}else{
 			redirect('Admin');
 		}
 	}
 
-	public function export_csv(){ 	
+	public function resetpass($key=""){
+		$wh=array(
+			"a.reset_token"=>$key,
+			"a.status"=>"aktif"
+		);
+		$var = $this->m_invest->checkPengguna($wh);
+		if($var->num_rows()){
+			$now = date("Y-m-d H:i:s");
+			$dt = $var->row();
+			$exp = $dt->reset_token!=""?$dt->reset_exp:date("Y-m-d H:i:s");
+			// Declare and define two dates
+			$date1 = strtotime($now);
+			$date2 = strtotime($exp);
+			$days = $this->dayCount($now,$exp);
+
+			if($days>=1 ){
+				// $result=array("alert"=>"danger",'title'=>"Gagal",'hasil'=>'Token Expire');
+				$result['notif'] = '<div class="alert alert-danger">
+								  <i class="fa fa-check-circle"></i>Token Expire
+							  </div>';
+				$this->session->set_flashdata($result);
+				redirect("admin");
+			} else {
+				$data['key']=$key;
+				$data['datatoken']=$dt;
+				//$data['dataPerguruanRow'] = $this->M_mhs->dataPerguruanTinggi()->row();
+				$this->load->view("admin/form-reset-pass", $data);
+				// $this->load->view('index',$data);
+			}
+		} else {
+			$result=array("alert"=>"danger",'title'=>"Gagal",'hasil'=>'Data not Found');
+			$result['notif'] = '<div class="alert alert-danger">
+				<i class="fa fa-times-circle"></i> Data Not Found
+			</div>';
+			$this->session->set_flashdata($result);
+			redirect("admin");
+		}
+	}
+
+	public function dayCount($now,$exp){
+		// Declare and define two dates
+		$date1 = strtotime($now);
+		$date2 = strtotime($exp);
+		// Formulate the Difference between two dates
+		$diff = abs($date2 - $date1);
+		// To get the year divide the resultant date into total seconds in a year (365*60*60*24)
+		$years = floor($diff / (365*60*60*24));
+		// To get the month, subtract it with years and divide the resultant date into total seconds in a month (30*60*60*24)
+		$months = floor(($diff - $years * 365*60*60*24)  / (30*60*60*24));
+		// To get the day, subtract it with years and months and divide the resultant date into total seconds in a days (60*60*24)
+		$days = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24)/ (60*60*24));
+		// To get the hour, subtract it with years, months & seconds and divide the resultant date into total seconds in a hours (60*60)
+		$hours = floor(($diff - $years * 365*60*60*24  - $months*30*60*60*24 - $days*60*60*24) / (60*60));
+		// To get the minutes, subtract it with years, months, seconds and hours and divide the resultant date into total seconds i.e. 60
+		$minutes = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24 - $days*60*60*24 - $hours*60*60)/ 60);
+		// To get the minutes, subtract it with years, months, seconds, hours and minutes
+		$seconds = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24 - $days*60*60*24 - $hours*60*60 - $minutes*60));
+		// Print the result
+		//echo $now." - ".$exp."</br>";
+		//printf("%d years, %d months, %d days, %d hours, ". "%d minutes, %d seconds", $years, $months, $days, $hours, $minutes, $seconds);
+		return $days;
+	}
+
+	public function export_csv(){
 		/* file name */
-		$filename = 'transection_'.date('Ymd').'.csv'; 
-		header("Content-Description: File Transfer"); 
-		header("Content-Disposition: attachment; filename=$filename"); 
+		$filename = 'transection_'.date('Ymd').'.csv';
+		header("Content-Description: File Transfer");
+		header("Content-Disposition: attachment; filename=$filename");
 		header("Content-Type: application/csv; ");
 	   /* get data */
 		$history = $this->m_invest->dataDanaHistoryTransaksiAdmin();
 		/* file creation */
 		$file = fopen('php://output', 'w');
-		$header = array("Username","Email","Tipe","Tipe User","Login From","Status","Investor Status"); 
+		$header = array("Username","Email","Tipe","Tipe User","Login From","Status","Investor Status");
 		fputcsv($file, $header);
-		foreach ($history as $key=>$line){ 
-			fputcsv($file,$line); 
+		foreach ($history as $key=>$line){
+			fputcsv($file,$line);
 		}
-		fclose($file); 
-		exit; 
+		fclose($file);
+		exit;
 	}
 
 }
--- application/controllers/Loginadmin.php
+++ application/controllers/Loginadmin.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 class Loginadmin extends CI_Controller {
-	
+
 	function __construct(){
 		parent::__construct();
         $this->load->helper('url');
@@ -15,13 +15,13 @@ class Loginadmin extends CI_Controller {
 
 	public function do_login()
 	{
-		
+
 		$data = array('email' => $this->input->post('email'),
 					'password' => md5($this->input->post('password'))
 					);
-		
+
 		$var = $this->M_admin->select($data);
-		
+
 		if ($var->num_rows() > 0 && ($var->row()->tipe == 'admin' || $var->row()->status == 'aktif')) {
 			$data_session = array(
 				'emails' => $this->input->post('email'),
@@ -57,11 +57,11 @@ class Loginadmin extends CI_Controller {
 			$now = date("Y-m-d H:i:s");
 			$dt = $var->row();
 			//$exp = $dt->RESET_TOKEN!=""?$dt->RESET_EXP:date("Y-m-d H:i:s");
-			// Declare and define two dates 
+			// Declare and define two dates
 			//$date1 = strtotime($now);
 			//$date2 = strtotime($exp);
 			//$days=$this->dayCount($now,$exp);
-			
+
 			//if($days>=1 || $dt->RESET_TOKEN==""){
 				$tomorrow = date('Y-m-d H:i:s',strtotime(date("Y-m-d H:i:s") . "+1 days"));
 				$dt=array(
@@ -74,7 +74,7 @@ class Loginadmin extends CI_Controller {
 				);
 				$this->m_invest->updatedata("tbl_admin",$dt,$wh);
 				$mail= $this->m_invest->kirimEmailnya($mailTo,$mailformat);
-				
+
 				if($mail=="success"){
 					$alert = '<div class="alert alert-success">
 								  <i class="fa fa-check-circle"></i>Email terkirim
@@ -93,18 +93,109 @@ class Loginadmin extends CI_Controller {
 						 Email Belum Terdaftar
 					  </div>';
 		}
-		$this->session->set_flashdata(array('notif' => $alert)); 
-		// $this->session->set_flashdata($result); 
+		$this->session->set_flashdata(array('notif' => $alert));
+		// $this->session->set_flashdata($result);
         //echo $mail;
 		//echo $mailformat;
         redirect("admin/reset_pass");
     }
 
+	function prosesResetPass(){
+		$key = $this->input->post("key");
+		$wh=array(
+			"a.reset_token"=>$key,
+			"a.status"=>"aktif"
+		);
+		$var = $this->m_invest->checkPengguna($wh);
+		if($var->num_rows()){
+			$now = date("Y-m-d H:i:s");
+			$dt = $var->row();
+			$exp = $dt->reset_token!=""?$dt->reset_exp:date("Y-m-d H:i:s");
+			// Declare and define two dates
+			$date1 = strtotime($now);
+			$date2 = strtotime($exp);
+			$days = $this->dayCount($now,$exp);
+			if($days>=1 ){
+				// $result=array("alert"=>"danger",'title'=>"Gagal",'hasil'=>'Token Expire');
+				$result['notif'] = '
+							  <div class="alert alert-danger">
+									<i class="fa fa-times-circle"></i> Token Expire
+							  </div>';
+				$this->session->set_flashdata($result);
+				redirect("admin");
+			} else {
+				$dt = $var->row();
+				$wh=array("email"=>$dt->mailto);
+				$npass=$this->input->post("npass");
+				$cpass=$this->input->post("cpass");
+				if($npass==$cpass){
+					$badpass = $npass;
+					$goodpass = md5($badpass);//password_hash($badpass, PASSWORD_BCRYPT);
+					$dt=array(
+						"reset_token"=>"",
+						"reset_exp"=>$now,
+						"password"=>$goodpass
+					);
+					//$upd = $this->M_user->updateAkun($dt, $wh);
+					$this->m_invest->updatedata("tbl_admin",$dt,$wh);
+					//if($upd>0){
+						// $result=array("alert"=>"success",'title'=>"Berhasil",'hasil'=>'Berhasil');
+						$result['notif'] = '<div class="alert alert-success">
+									<i class="fa fa-check-circle"></i> Berhasil Reset
+							  </div>';
+					/* } else {
+						$result=array("alert"=>"danger",'title'=>"Gagal",'hasil'=>'Data Invalid');
+					} */
+					$this->session->set_flashdata($result);
+					redirect("admin");
+				} else {
+					// $result=array("alert"=>"danger",'title'=>"Gagal",'hasil'=>'Data Pass & Konf harus Sama');
+					$result['notif'] = '<div class="alert alert-danger">
+								<i class="fa fa-times-circle"></i> Password tidak sama
+							</div>';
+					$this->session->set_flashdata($result);
+					redirect("admin/resetpass/".$key);
+				}
+			}
+		} else {
+			// $result=array("alert"=>"danger",'title'=>"Gagal",'hasil'=>'Data not Found');
+			$result['notif'] = '<div class="alert alert-danger">
+						<i class="fa fa-times-circle"></i> Data Not Found
+					</div>';
+			$this->session->set_flashdata($result);
+			redirect("admin");
+		}
+	}
+
 	function logout(){
 		unset($_SESSION['id_admins'],$_SESSION['emails'],$_SESSION['passwords'],$_SESSION['tipe']);
-		
+
 		// redirect('admin_menstruasi');
 		redirect('Admin');
+	}
+
+	public function dayCount($now,$exp){
+		// Declare and define two dates
+		$date1 = strtotime($now);
+		$date2 = strtotime($exp);
+		// Formulate the Difference between two dates
+		$diff = abs($date2 - $date1);
+		// To get the year divide the resultant date into total seconds in a year (365*60*60*24)
+		$years = floor($diff / (365*60*60*24));
+		// To get the month, subtract it with years and divide the resultant date into total seconds in a month (30*60*60*24)
+		$months = floor(($diff - $years * 365*60*60*24)  / (30*60*60*24));
+		// To get the day, subtract it with years and months and divide the resultant date into total seconds in a days (60*60*24)
+		$days = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24)/ (60*60*24));
+		// To get the hour, subtract it with years, months & seconds and divide the resultant date into total seconds in a hours (60*60)
+		$hours = floor(($diff - $years * 365*60*60*24  - $months*30*60*60*24 - $days*60*60*24) / (60*60));
+		// To get the minutes, subtract it with years, months, seconds and hours and divide the resultant date into total seconds i.e. 60
+		$minutes = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24 - $days*60*60*24 - $hours*60*60)/ 60);
+		// To get the minutes, subtract it with years, months, seconds, hours and minutes
+		$seconds = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24 - $days*60*60*24 - $hours*60*60 - $minutes*60));
+		// Print the result
+		//echo $now." - ".$exp."</br>";
+		//printf("%d years, %d months, %d days, %d hours, ". "%d minutes, %d seconds", $years, $months, $days, $hours, $minutes, $seconds);
+		return $days;
 	}
 
 	function generate_string($strength = 16) {
@@ -134,7 +225,7 @@ class Loginadmin extends CI_Controller {
             $data['mailformat'] = $this->input->post("format");
 			return $this->load->view('template/v-mail-format',$data,TRUE);
 		/* }else{
-			redirect("home/login");	
+			redirect("home/login");
 		} */
     }
 }
