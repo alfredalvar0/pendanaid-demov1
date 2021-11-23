30bf26ef13376a78df6deef58f0bcb4574671158
Arya Wahyu <arya.wahyu502@gmail.com>
2021-11-23 23:15:55 +0700

    update layouts, footer navbar dashboard & remove unused code

--- application/views/index.php
+++ application/views/index.php
@@ -1,145 +1,98 @@
-<?php
-
-$useragent = $_SERVER['HTTP_USER_AGENT'];
-$ismobile = false;
-
-//cek perangkat
-$iPod = stripos($useragent, "iPod"); 
-$iPad = stripos($useragent, "iPad"); 
-$iPhone = stripos($useragent, "iPhone");
-$Android = stripos($useragent, "Android"); 
-$iOS = stripos($useragent, "iOS");
-$DEVICE = ($iPod||$iPad||$iPhone||$Android||$iOS);
-//cek perangkat
-
-if(preg_match('/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i',$useragent)||preg_match('/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i',substr($useragent,0,4))
-    && ($DEVICE=true) 
-  )
-{
-   $ismobile = true; 
-}
-
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
-<?php
-    $this->load->view("template/head");
-?>
+<?php $this->load->view("template/head");?>
 
 <body>
-
-  <?php
-  if($this->session->userdata("invest_username")!="" && $this->session->userdata("invest_email")!=""){
-    if($this->router->fetch_method() != "dokumen_saya" && 
-      $this->router->fetch_method() != "agreement" &&
-      $this->router->fetch_method() != "perjanjiananggota" &&
-      $this->router->fetch_method() != "perjanjianpinjaman" &&
-      $this->router->fetch_method() != "laporanhistory" &&
-      $this->router->fetch_method() != "laporanbisnis" &&
-      $this->router->fetch_method() != "laporanjual" &&
-      $this->router->fetch_method() != "jual" &&
-      $this->router->fetch_method() != "proyeksi" &&
-      $this->router->fetch_method() != "kode_referral" &&
-      $this->router->fetch_method() != "bank_account" &&
-      $this->router->fetch_method() != "erups" &&
-      $this->router->fetch_method() != "login" &&
-      ($this->router->fetch_class() == "bisnis" && $this->router->fetch_method() == "register") &&
-      $this->router->fetch_method() != "role_choice" &&
-      $this->router->fetch_method() != "login_choice" &&
-      $this->router->fetch_method() != "register_choice" &&
-      $this->router->fetch_method() != "evote") {
-      $this->load->view("template/header_investor");
-    }
-  } else{
-    if($this->router->fetch_method() != "login" &&
-      $this->router->fetch_method() != "role_choice" &&
-      $this->router->fetch_method() != "login_choice" &&
-      ($this->router->fetch_class() == "bisnis" && $this->router->fetch_method() == "register") &&
-      $this->router->fetch_method() != "register_choice") {
-      $this->load->view("template/header");
-    }
-  }
-?>
-  
-  <main id="main"><!-- toggled-->
-      <?php
-      if($this->session->userdata("invest_username")!="" && $this->session->userdata("invest_email")!=""){
-        if($ismobile == false){
-            //$this->load->view("template/sidebar_investor");
+  <?php if($this->session->userdata("invest_username")!="" && $this->session->userdata("invest_email")!=""){ 
+    if(strtolower($this->router->fetch_class()) == "investor") {
+      if(strtolower($this->router->fetch_method()) == "dokumen_saya" ||
+        strtolower($this->router->fetch_method()) == "agreement" ||
+        strtolower($this->router->fetch_method()) == "perjanjiananggota" ||
+        strtolower($this->router->fetch_method()) == "perjanjianpinjaman" ||
+        strtolower($this->router->fetch_method()) == "laporanbisnis" ||
+        strtolower($this->router->fetch_method()) == "laporanjual" ||
+        strtolower($this->router->fetch_method()) == "jual" ||
+        strtolower($this->router->fetch_method()) == "proyeksi" ||
+        strtolower($this->router->fetch_method()) == "kode_referral" ||
+        strtolower($this->router->fetch_method()) == "bank_account" ||
+        strtolower($this->router->fetch_method()) == "evote" ||
+        strtolower($this->router->fetch_method()) == "erups" ||
+        strtolower($this->router->fetch_method()) == "laporanhistory") {} 
+        else {
+          $this->load->view("template/header_investor");
         }
+      } else {
+        $this->load->view("template/header_investor");
       }
+    } else {
+      if(strtolower($this->router->fetch_class()) == "invest") {
+        if(strtolower($this->router->fetch_method()) != "login" &&
+        strtolower($this->router->fetch_method()) != "role_choice" &&
+        strtolower($this->router->fetch_method()) != "login_choice" &&
+        strtolower($this->router->fetch_method()) != "register_choice" &&
+        strtolower($this->router->fetch_method()) != "register") {
+          $this->load->view("template/header");
+        }
+      } else {
+          $this->load->view("template/header");
+      }
+    }
+  ?>
+  
+  <main id="main">
+  <?php 
     $this->load->view("template/foot");
     echo $content;
-    
-    
-?>
-    
+  ?>
+
   </main>
-<?php
-  if($this->router->fetch_method() != "dokumen_saya" && 
-  $this->router->fetch_method() != "agreement" &&
-  $this->router->fetch_method() != "perjanjiananggota" &&
-  $this->router->fetch_method() != "perjanjianpinjaman" &&
-  $this->router->fetch_method() != "laporanhistory" &&
-  $this->router->fetch_method() != "laporanbisnis" &&
-  $this->router->fetch_method() != "laporanjual" &&
-  $this->router->fetch_method() != "jual" &&
-  $this->router->fetch_method() != "proyeksi" &&
-  $this->router->fetch_method() != "kode_referral" &&
-  $this->router->fetch_method() != "bank_account" &&
-  $this->router->fetch_method() != "erups" &&
-  $this->router->fetch_method() != "login" &&
-  ($this->router->fetch_class() == "bisnis" && $this->router->fetch_method() == "register") &&
-  $this->router->fetch_method() != "role_choice" &&
-  $this->router->fetch_method() != "login_choice" &&
-  $this->router->fetch_method() != "register_choice" &&
-  $this->router->fetch_method() != "evote") {
-    $this->load->view("template/footer");
-  }
-?>
-  
-<?php
-    
-    if($this->session->userdata("invest_username")!="" && $this->session->userdata("invest_email")!="" ){
-?>
+  <?php
+    if($this->session->userdata("invest_username") !="" && $this->session->userdata("invest_email") !=""){
+      if(strtolower($this->router->fetch_class()) == "investor") {
+        if(strtolower($this->router->fetch_method()) == "dokumen_saya" ||
+        strtolower($this->router->fetch_method()) == "agreement" ||
+        strtolower($this->router->fetch_method()) == "perjanjiananggota" ||
+        strtolower($this->router->fetch_method()) == "perjanjianpinjaman" ||
+        strtolower($this->router->fetch_method()) == "laporanbisnis" ||
+        strtolower($this->router->fetch_method()) == "laporanjual" ||
+        strtolower($this->router->fetch_method()) == "jual" ||
+        strtolower($this->router->fetch_method()) == "proyeksi" ||
+        strtolower($this->router->fetch_method()) == "kode_referral" ||
+        strtolower($this->router->fetch_method()) == "bank_account" ||
+        strtolower($this->router->fetch_method()) == "evote" ||
+        strtolower($this->router->fetch_method()) == "erups" ||
+        strtolower($this->router->fetch_method()) == "laporanhistory") {} 
+        else {
+          $this->load->view("template/footer");
+        }
+      } else {
+        $this->load->view("template/header_investor");
+      }
+    } else {
+      if(strtolower($this->router->fetch_class()) == "invest") {
+        if(strtolower($this->router->fetch_method()) != "login" &&
+          strtolower($this->router->fetch_method()) != "role_choice" &&
+          strtolower($this->router->fetch_method()) != "login_choice" &&
+          strtolower($this->router->fetch_method()) != "register_choice" &&
+          strtolower($this->router->fetch_method()) != "register") {
+            $this->load->view("template/footer");
+        }
+      } else {
+          $this->load->view("template/footer");
+      }
+    } ?>
 
-        <!-- <script type="text/javascript">
-            $(document).ready(function(){
-                //$("#main").toggleClass("toggled");
-                $('[data-toggle="sidebar"]').click(function(e) {
-                    e.preventDefault();
-                    $("#main").toggleClass("toggled");
-                });
-            });
-        </script> -->
-<?php
-    }
-?>
-	<script>
-		$(document).ready(function(){
-			$("a[href='#top']").click(function() {
-			  $("html, body").animate({ scrollTop: 0 }, "slow");
-			  return false;
-			});
-		});
-	</script>
-	
-	
-	
-<!--notifikasi-->
-<script>
-<?php 
-$msg = $this->session->flashdata('message');
-if($msg=="success"){?>
-Swal.fire( 'Sukses', 'Proses berhasil!',  'success' );
-<?php }else if($msg=="failed"){?>
-Swal.fire( 'Gagal', 'Proses gagal!',  'error' );
-<?php }else if($msg=="failed_saldo"){?>
-Swal.fire( 'Gagal', 'Saldo anda tidak cukup!',  'error' );
-<?php } ?>
-</script>
+  <script>
+    <?php 
+    $msg = $this->session->flashdata('message');
+    if($msg=="success"){?>
+    Swal.fire( 'Sukses', 'Proses berhasil!',  'success' );
+    <?php }else if($msg=="failed"){?>
+    Swal.fire( 'Gagal', 'Proses gagal!',  'error' );
+    <?php }else if($msg=="failed_saldo"){?>
+    Swal.fire( 'Gagal', 'Saldo anda tidak cukup!',  'error' );
+    <?php } ?>
+  </script>
 
-    </body>
+</body>
 </html>
