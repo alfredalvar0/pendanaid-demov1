5799ba233f7f21e5bcb6d496a26c2fc7b9f5f949
kcraksa <kciptaraksa@gmail.com>
2022-01-12 17:02:33 +0700

    ubah config base_url

--- application/config/config.php
+++ application/config/config.php
@@ -27,7 +27,7 @@ date_default_timezone_set('Asia/Jakarta');
 //$config['base_url'] = 'http://pendanausaha.sekolahpilotfilipina.com/';
 
 //dinamis bisa menggunakan http atau https
-$config['base_url'] =  ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ?  "https" : "http");
+$config['base_url'] =  (((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") || $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') ?  "https" : "http");
 $config['base_url'] .=  "://".$_SERVER['HTTP_HOST'];
 $config['base_url'] .=  str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 
